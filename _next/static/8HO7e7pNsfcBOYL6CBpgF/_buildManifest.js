4c5920010a7f36a6519f460c2e38d432d82f9c24
nonolooklook <nonolooklook@users.noreply.github.com>
2022-05-19 13:51:19 +0000

    deploy: onelandworld/oneland-home@ddf4a77a0b9c2f1b935952b64bb45822e8ea2c4a

--- _next/static/8HO7e7pNsfcBOYL6CBpgF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a5e1755f91f79474.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/AuLy-r4pKS23MeSMcZI9A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-338e2415b12337c2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
