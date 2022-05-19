b7297d1084a7fb3679c0885b3b722ab8d3d7050e
lijun4639 <lijun4639@users.noreply.github.com>
2022-05-19 11:15:22 +0000

    deploy: onelandworld/oneland-home@990b5e9a696f4a0af7feee852a9bedac62ce253a

--- _next/static/I6dc_pmfJmwUOa4MbLuJ1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b087d8f11f224e17.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
